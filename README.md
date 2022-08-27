659230f07e5fb8a53371caa0b5ecd4f9a295b925
Taejun Kim <taejun@kaist.ac.kr>
2022-08-27 23:13:18 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# djmix-dataset
+# The DJ Mix Dataset
+
+The DJ Mix dataset contains metadata of DJ mixes played by human DJs and played tracks in the mixes.
---
+++ scripts/download.py
@@ -0,0 +1,81 @@
+import os
+import logging
+import json
+import traceback
+from yt_dlp import YoutubeDL
+
+DATASET_DIR = './dataset'
+MIX_DIR = mkpath(DATASET_DIR, 'mixes')
+TRACK_DIR = mkpath(DATASET_DIR, 'tracks')
+
+
+def main():
+  logging.basicConfig(level=logging.INFO)
+  
+  os.makedirs(MIX_DIR, exist_ok=True)
+  os.makedirs(TRACK_DIR, exist_ok=True)
+  
+  mixes = json.load(open(mkpath(DATASET_DIR, 'djmix-dataset.json')))
+  
+  for mix in mixes:
+    try:
+      download_mix(mix)
+    except Exception as e:
+      logging.error(f'Failed to download {mix["id"]}')
+      traceback.print_exc()
+    
+    for track in mix['tracklist']:
+      try:
+        if track["id"] is None:
+          continue
+        download_track(track)
+      except Exception as e:
+        logging.error(f'Failed to download track: https://www.youtube.com/watch?v={track["id"]}')
+        traceback.print_exc()
+
+
+def download_mix(mix):
+  logging.info(f'=> Start downloading {mix["id"]}')
+  
+  download_audio(
+    url=mix['audio_url'],
+    path=mkpath(MIX_DIR, f'{mix["id"]}.mp3')
+  )
+
+
+def download_track(track):
+  url = f'https://www.youtube.com/watch?v={track["id"]}'
+  logging.info(f'=> Start downloading track {url}')
+  
+  track_path = mkpath(TRACK_DIR, track["id"][0], f'{track["id"]}.mp3')
+  os.makedirs(os.path.dirname(track_path), exist_ok=True)
+  
+  download_audio(
+    url=url,
+    path=track_path,
+  )
+
+
+def download_audio(url, path):
+  if os.path.isfile(path):
+    logging.info(f'{path} already exists. Skip downloading.')
+    return
+  
+  params = {
+    'format': 'bestaudio',
+    'outtmpl': path,
+    'postprocessors': [{  # Extract audio using ffmpeg
+      'key': 'FFmpegExtractAudio',
+      'preferredcodec': 'mp3',
+    }]
+  }
+  with YoutubeDL(params) as ydl:
+    ydl.download(url)
+
+
+def mkpath(*paths):
+  return os.path.realpath(os.path.join(*paths))
+
+
+if __name__ == '__main__':
+  main()
